b0db8fe1bdeb5221bd2c6b116bc4e7f14badec09
anton-kabantsev <61785564+anton-kabantsev@users.noreply.github.com>
2022-04-19 22:23:54 +1000

    it's done now python can connect to 1C via com connector, returns following output: Бухгалтерия предприятия, редакция 3.0 3.0.106.60 8.3.20.1674

--- 1c_get_bd_ver.py
+++ 1c_get_bd_ver.py
@@ -1,9 +1,12 @@
 import win32com.client
 CONN_STRING = r'File="C:\Users\Антон\Documents\1C\DemoAccounting";Usr="АбрамовГС (директор)";'
-buh = win32com.client.Dispatch("V83.ComConnector")
-buh.connect(CONN_STRING)
-SystemInfo = buh.NewObject("Query", ' ')
-print(SystemInfo.AppVersion)
+com = win32com.client.Dispatch("V83.ComConnector")
+buh = com.connect(CONN_STRING)
+#SystemInfo = buh.NewObject("Query", ' ')
+system = buh.NewObject("SystemInfo")
+print(buh.Метаданные.Синоним)
+print(buh.Метаданные.Версия)
+print(system.ВерсияПриложения)
 # trade = cntr.Connect («File=""c:\InfoBases\Trade»»; Usr=""Director»»;»)     'Получить внешнее соединение
 # trade_goods = trade.Справочники.Товары
 # trade_goods = СправочникТоваров.СоздатьГруппу()
